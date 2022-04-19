f0d06beb0b23ed83a206909969d4d91fe5eaa3b3
Andrea Cristina Cavero Arrivasplata <andrea.cavero@utec.edu.pe>
2022-04-19 16:11:48 -0500

    mri_changes

--- Codigos/SegmentacionMRI.m
+++ Codigos/SegmentacionMRI.m
@@ -19,28 +19,28 @@ f2=imresize(f2,0.85);
 figure(1)
 imshow(f2,[]);
 
-%% Thresholding
-
-seg1 = f > 0.5;
-imshow(seg1,[])
-imshow(seg1.*f,[])
-seg1 = f < 0.75;
-imshow(seg1,[])
-imshow(seg1.*f,[])
-
-imhist(f)
-% Use a third threshold based on the histogram
-%% 
 %% Otsu method
 
-thr = graythresh(f)
-seg1 = f > thr;
+%imagen 1
+thr1 = graythresh(f1);
+seg1 = f1 > thr1;
 imshow(seg1,[])
-dxp=[0,1;-1,0];
-dyp=[1,0;0,-1];
-edgemap = abs(conv2(seg1,dxp,'same'))+abs(conv2(seg1,dyp,'same'));
-imshow(f+edgemap,[0,1]);
-% Compare the otsu provided threshold vs the one you selected in the
-% preview step.
-% Do you trust the Otsu treshold?
-% Select your own image and compute the otsu
+dxp1=[0,1;-1,0];
+dyp1=[1,0;0,-1];
+edgemap = abs(conv2(seg1,dxp1,'same'))+abs(conv2(seg1,dyp1,'same'));
+imshow(f1+edgemap,[0,1]);
+
+%imagen 2
+thr2 = graythresh(f2);
+seg2 = f2 > thr2;
+imshow(seg2,[])
+dxp2=[0,1;-1,0];
+dyp2=[1,0;0,-1];
+edgemap = abs(conv2(seg2,dxp2,'same'))+abs(conv2(seg2,dyp2,'same'));
+imshow(f2+edgemap,[0,1]);
+
+%% K Means
+
+
+
+%% 
